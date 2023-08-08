15f93ff293864a3737799b29cd4fb94c875cc0f8
AChhatrapati <ee22btech11012@iith.ac.in>
2023-08-08 18:42:32 +0530

    1ST

--- codes/assign.py
+++ codes/assign.py
@@ -13,11 +13,8 @@ def AreaCalc(A, B, C):
     return area
 
 # Given vertices
-A = [1, -1]
-B = [-4, 6]
-C = [-3, -5]
-A = np.array(A)
-B = np.array(B)
-C = np.array(C)
+A = np.array([1,-1])
+B = np.array([-4,6])
+C = np.array([-3,-5])
 area_ABC = AreaCalc(A, B, C)
 print("Area of triangle ABC:", area_ABC)
