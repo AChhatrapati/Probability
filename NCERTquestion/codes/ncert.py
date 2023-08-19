abf212fb7f2b596e19797a276b2d0385207812ed
AChhatrapati <ee22btech11012@iith.ac.in>
2023-08-19 10:57:31 +0530

    ncert1

--- NCERTquestion/codes/ncert.py
+++ NCERTquestion/codes/ncert.py
@@ -1,20 +1,20 @@
 # Given probabilities
-# P(hindi) = P_H
-# P(english) = P_E
+# P(hindi) = p_H
+# P(english) = p_E
 
-P_H = 0.60
-P_E = 0.40
-P_both = 0.20
+p_H = 0.60
+p_E = 0.40
+p_both = 0.20
 
 # (a) Probability that she reads neither Hindi nor English newspapers
-P_neither = 1 - (P_H + P_E - P_both)
-print("(a) Probability that she reads neither Hindi nor English newspapers:", P_neither)
+p_neither = 1 - (p_H + p_E - p_both)
+print("(a) Probability that she reads neither Hindi nor English newspapers:", p_neither)
 
-# (b) Probability that she reads English newspaper given that she reads Hindi newspaper=P_E_H
-P_E_H = P_both / P_H
-print("(b) Probability that she reads English newspaper given Hindi newspaper:", P_E_H)
+# (b) Probability that she reads English newspaper given that she reads Hindi newspaper=p_E_H
+p_E_H = p_both / p_H
+print("(b) Probability that she reads English newspaper given Hindi newspaper:", p_E_H)
 
-# (c) Probability that she reads Hindi newspaper given that she reads English newspaper=P_H_E
-P_H_E = P_both / P_E
-print("(c) Probability that she reads Hindi newspaper given English newspaper:", P_H_E)
+# (c) Probability that she reads Hindi newspaper given that she reads English newspaper=p_H_E
+p_H_E = p_both / p_E
+print("(c) Probability that she reads Hindi newspaper given English newspaper:", p_H_E)
 
